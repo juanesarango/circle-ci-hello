c45c06fdc16cb1c5abc6e6582793457146e472d7
Juan Esteban Arango <juanes.ao@gmail.com>
2017-10-19 14:46:03 -0500

    Fix ssh key

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -6,13 +6,9 @@ ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
     
 cat > ~/.netrc << EOF
 machine git.heroku.com
-login $HEROKU_LOGIN
-password $HEROKU_API_KEY
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
 EOF
-
-cat >> ~/.ssh/config << EOF
-VerifyHostKeyDNS yes
-StrictHostKeyChecking no
 
 # Add heroku.com to the list of known hosts
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
