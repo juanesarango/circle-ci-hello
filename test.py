e0df4de8bba76689cca5796ff0e86e447a43a583
Juan Esteban Arango <juanes.ao@gmail.com>
2017-10-18 20:47:28 -0500

    Add flask app

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+import json
+from index import app
+
+
+class TestFlaskApi(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(json.loads(response.get_data()), {'hello': 'world'})
+
+if __name__ == '__main__':
+    unittest.main()
