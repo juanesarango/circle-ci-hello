6d69219fa39143635b7ed8f9514b997ab16c19e7
Juan Esteban Arango <juanes.ao@gmail.com>
2017-10-19 14:40:48 -0500

    Fix ssh key

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -5,7 +5,7 @@
   ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
      
   cat > ~/.netrc << EOF
-  machine api.heroku.com
+  machine git.heroku.com
     login $HEROKU_LOGIN
     password $HEROKU_API_KEY
   EOF
